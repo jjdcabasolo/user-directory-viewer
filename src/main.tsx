4a192265873dda29050afbcd7f724f1b2cff7683
jjdcabasolo <abasolojohnjourish@gmail.com>
2025-09-04 13:07:31 +0000

    feat: add responsive styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
